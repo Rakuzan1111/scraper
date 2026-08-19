8d0d05e225d02470a0f3d46183ae418c2f09c610
Eric Song <ericsong@Mac.ht.home>
2026-08-19 19:57:35 -0400

    Load rental listings from CSV instead of c++

--- dashboard/src/main.cpp
+++ dashboard/src/main.cpp
@@ -1,10 +1,85 @@
+//standard libraries
 #include <iostream>
 #include <vector>
+#include <fstream> //file stream: file -> program
+#include <sstream>
+#include <string>
 
+//files
 #include "listing.hpp"
 
 // =======================================================================
 
+// takes a string and creates a vector (list = python equivalent) of the elements separated by commas
+// ex. "I,am,John,Doe" -> {I, am, John, Doe}
+std::vector<std::string> splitLine(const std::string& line)
+{
+    std::vector<std::string> fields;
+    std::stringstream stream {line};
+    std::string field;
+
+    while (std::getline(stream, field, ','))
+    {
+        fields.push_back(field);
+    }
+
+    return fields;
+}
+
+// =======================================================================
+
+// receives a filename and returns a vector of listing objects
+std::vector<Listing> loadListingsFromCsv(const std::string& filename)
+{
+    std::ifstream file {filename};
+
+    if (!file)
+    {
+        std::cerr << "Could not open file: " //cerr for error messages
+                  << filename
+                  << '\n';
+
+        return {};
+    }
+
+    std::vector<Listing> listings;
+
+    std::string line;
+
+    std::getline(file, line); //initial read for the first line as it is not necessary (column names)
+
+    while (std::getline(file, line))
+    {
+        std::vector<std::string> fields {
+            splitLine(line)
+        };
+
+        if (fields.size() != 10)
+        {
+            std::cerr << "Skipping invalid CSV row.\n";
+            continue; // keyword to stop current loop and start next iteration 
+        }
+
+        Listing listing;
+
+        listing.id = fields[0];
+        listing.source = fields[1];
+        listing.title = fields[2];
+        listing.price = std::stod(fields[3]); //stod = string to double
+        listing.previousPrice = std::stod(fields[4]);
+        listing.neighborhood = fields[5];
+        listing.city = fields[6];
+        listing.bedrooms = std::stoi(fields[7]); //stoi = string to integer
+        listing.bathrooms = std::stod(fields[8]);
+        listing.url = fields[9];
+
+        listings.push_back(listing);
+    }
+
+    return listings;
+}
+
+// =======================================================================
 // calculates the average price of the apartments
 double calculateAveragePrice(const std::vector<Listing>& listings)
 {
@@ -61,7 +136,7 @@ std::vector<Listing> filterByMaxPrice(const std::vector<Listing>& listings, doub
 // =======================================================================
 int main()
 {
-
+/*
     // list of apartment details (dummy information)
 
 
@@ -110,12 +185,16 @@ int main()
 
     // ===================================================================
 
+
     // print details of the apartments
     std::vector<Listing> listings;
     listings.push_back(apartment);
     listings.push_back(apartment2);
     listings.push_back(apartment3);
-
+*/
+    std::vector<Listing> listings {
+    loadListingsFromCsv("dashboard/data/listings.csv")
+    };
     // asks the user their maximum price to filter out apartments above the rent price
     double maxPrice {};
     std::cout << "\nEnter maximum rent: ";
