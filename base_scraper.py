fbe8c9655be89a84b7a765debad4dc8d09f5dd53
Runzhen Yu <runzhen2007.yu@gmail.com>
2026-08-27 17:33:13 -0400

    added logs and other improvements

--- base_scraper.py
+++ base_scraper.py
@@ -2,6 +2,15 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 import re
+import logging
+from logging.handlers import RotatingFileHandler
+import time
+
+handler = RotatingFileHandler(filename = 'logs.txt', maxBytes=1_000_000, backupCount=3)
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s:%(levelname)s:%(message)s', handlers=[handler])
+logging.info('New entry: ----------------------------------------------------------------------')
+
+
 
 # Headers that makes kijiji recognize requests as human requests
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
@@ -21,8 +30,9 @@ headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/
 # Get initial response to see if website is responding
 response = requests.get('https://www.kijiji.ca/b-a-louer/grand-montreal/c30349001l80002', headers=headers)
 
-if not response:
-    print('No response', response.status_code)
+if response.status_code != 200:
+    logging.error(f'Initial request failed: {response.status_code}')
+    print('Request failed:', response.status_code)
     exit()
 
 # Listing attributes
@@ -47,57 +57,76 @@ with open("listings.csv", 'w', newline='', encoding='UTF-8-SIG') as f:
     base_url = 'https://www.kijiji.ca/b-a-louer/grand-montreal/{page_part}c30349001l80002'
 
     for page in range(1, 30):
+        time.sleep(1)
         # Changes Url depending on page number
         page_part = '' if page == 1 else f'page-{page}/'
         url = base_url.format(page_part=page_part)
-        response = requests.get(url, headers=headers, params={'view': 'list'})
+
+        try:
+            response = requests.get(url, headers=headers, params={'view': 'list'}, timeout=10)
+            response.raise_for_status()
+
+        except requests.exceptions.RequestException as e:
+            logging.error(f"Failed to fetch page {page}: {e}")
+            continue  
 
         # Parses html and extracts all "card (section)" objects    
         soup = BeautifulSoup(response.text, 'html.parser')
         cards = soup.find_all('section', attrs={'data-testid': 'listing-card'})
-        
-        # Gets information from the extracted cards
-        for information in cards:
-            listing = information.find('a', attrs={'data-testid': 'listing-link'})
-            website = listing.get('href')
-            listing_id = website.split('/')[-1]
             
-            # Filters duplicates with set()
-            if website in seen:
-                duplicate += 1
+        # Gets information from the extracted cards
+        
+        for information in cards:
+            listing_id = None
+            try:
+                listing = information.find('a', attrs={'data-testid': 'listing-link'})
+                website = listing.get('href')
+                listing_id = website.split('/')[-1]
+                
+                # Filters duplicates with set()
+                if website in seen:
+                    duplicate += 1
+                    logging.debug(f'Listing {listing_id} is already in listings. Skip over.')
+                    continue
+
+                seen.add(website)
+                new += 1
+
+                #Base attributes that all listings have (formatted with re)
+                price = information.find('p', attrs={'data-testid': 'listing-price'}).text
+                price = re.sub(r'[$,]', '', price)
+
+                description = information.find('a', attrs={'data-testid': 'listing-link'}).text
+
+                location = information.find('div', attrs={'data-testid': 'listing-details'}).text
+                
+                # More information that can be present on the listing
+                further_information = information.find('ul', attrs={'data-testid': 're-attribute-list-non-mobile'})
+                    
+                data = {'price': price,
+                        'description': description,
+                        'location': location
+                        }    
+                    
+                if further_information:
+                    for name, label in fields.items():
+                        element = further_information.find('li', attrs={'aria-label': label})
+                        data[name] = element.get_text() if element else 'N/A'
+
+                else:
+                    for name in fields.keys():
+                        data[name] = 'N/A'
+
+                if data['size'] != 'N/A':
+                    data['size'] = re.search(r'\d+', data['size']).group()
+
+                writer.writerow([listing_id] + list(data.values()) + [website])
+                logging.debug(f'Added listing {listing_id} to listings')
+
+            except Exception as e:
+                logging.error(f"Error Occured on page {page} with listing {listing_id}: {e}")
                 continue
 
-            seen.add(website)
-            new += 1
-
-            #Base attributes that all listings have (formatted with re)
-            price = information.find('p', attrs={'data-testid': 'listing-price'}).text
-            price = re.sub(r'[$,]', '', price)
-
-            description = information.find('a', attrs={'data-testid': 'listing-link'}).text
-
-            location = information.find('div', attrs={'data-testid': 'listing-details'}).text
-            
-            # More information that can be present on the listing
-            further_information = information.find('ul', attrs={'data-testid': 're-attribute-list-non-mobile'})
-                 
-            data = {'price': price,
-                    'description': description,
-                    'location': location
-                    }    
-                
-            if further_information:
-                for name, label in fields.items():
-                    element = further_information.find('li', attrs={'aria-label': label})
-                    data[name] = element.get_text() if element else 'N/A'
-
-            else:
-                for name in fields.keys():
-                    data[name] = 'N/A'
-            if data['size'] != 'N/A':
-                data['size'] = re.search(r'\d+', data['size']).group()
-
-            writer.writerow([listing_id] + list(data.values()) + [website])
 
 print('Unique listings: ', new) 
 print('Duplicated(Removed) listings: ', duplicate)       
