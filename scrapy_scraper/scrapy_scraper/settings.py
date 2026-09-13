e805ee4862726c97d8ab66bff3949320d1dd3637
Runzhen Yu <runzhen2007.yu@gmail.com>
2026-09-13 13:28:17 -0400

    added rotating user agents to avoid automated traffic detection by simulating real users

--- scrapy_scraper/scrapy_scraper/settings.py
+++ scrapy_scraper/scrapy_scraper/settings.py
@@ -62,8 +62,10 @@ CONCURRENT_REQUESTS_PER_DOMAIN: 1
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    "scrapy_scraper.middlewares.ScrapyScraperDownloaderMiddleware": 543,
+DOWNLOADER_MIDDLEWARES = {
+    'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
+    'scrapy_user_agents.middlewares.RandomUserAgentMiddleware': 400,
+}
 #}
 
 # Enable or disable extensions
